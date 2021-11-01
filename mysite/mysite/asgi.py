8eb0ddbffcec61dc8c7d402d7d0a2dd14309d897
Leon <leopard10000@gmail.com>
2021-11-01 11:44:42 +0900

    Redisの有効化
    https://www.hiramine.com/programming/chat_django_channels/04_integrate_redis.html

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -18,6 +18,11 @@ django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter
 
+from channels.routing import URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
 application = ProtocolTypeRouter( {
     'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack( URLRouter( chat.routing.websocket_urlpatterns ) ),
 } )
