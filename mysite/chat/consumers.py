ecb8032dbdef03a499f4f44f67d89db826d245f5
Leon <leopard10000@gmail.com>
2021-11-02 10:06:12 +0900

    入退室ユーザー名、参加人数の表示
    https://www.hiramine.com/programming/chat_django_channels/09_system_message.html

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -4,12 +4,20 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 #from asgiref.sync import async_to_sync  # async_to_sync() : 非同期関数を同期的に実行する際に使用する。
 import datetime
 
+USERNAME_SYSTEM = '*system*'
+
 # ChatConsumerクラス: WebSocketからの受け取ったものを処理するクラス
 class ChatConsumer( AsyncWebsocketConsumer ):
+
+    # ルーム管理（インスタンス変数ではなく、インスタンス間で使用可能なクラス変数）
+    rooms = None
 
     # コンストラクタ
     def __init__( self, *args, **kwargs ):
         super().__init__( *args, **kwargs )
+        # クラス変数の初期化（最初のインスタンスが生成されたときのみ実施する）
+        if ChatConsumer.rooms is None:
+            ChatConsumer.rooms = {} # 空の連想配列
         self.strGroupName = ''
         self.strUserName = ''
 
@@ -80,6 +88,24 @@ class ChatConsumer( AsyncWebsocketConsumer ):
         self.strGroupName = 'chat_%s' % strRoomName
         await self.channel_layer.group_add( self.strGroupName, self.channel_name )
 
+        # 参加者数の更新
+        room = ChatConsumer.rooms.get(self.strGroupName)
+        if( None == room ):
+            # ルーム管理にルーム追加
+            ChatConsumer.rooms[self.strGroupName] = {'participants_count': 1}
+        else:
+            room['participants_count'] += 1
+        # システムメッセージの作成
+        strMessage = '"' + self.strUserName + '" joined. there are ' + str( ChatConsumer.rooms[self.strGroupName]['participants_count'] ) + ' participants'
+        # グループ内の全コンシューマーにメッセージ拡散送信（受信関数を'type'で指定）
+        data = {
+            'type': 'chat_message', # 受信処理関数名
+            'message': strMessage, # メッセージ
+            'username': USERNAME_SYSTEM, # ユーザー名
+            'datetime': datetime.datetime.now().strftime( '%Y/%m/%d %H:%M:%S' ), # 現在時刻
+        }
+        await self.channel_layer.group_send( self.strGroupName, data )
+
     # チャットからの離脱
     async def leave_chat( self ):
         if( '' == self.strGroupName ):
@@ -88,5 +114,23 @@ class ChatConsumer( AsyncWebsocketConsumer ):
         # グループから離脱
         await self.channel_layer.group_discard( self.strGroupName, self.channel_name )
 
+        # 参加者数の更新
+        ChatConsumer.rooms[self.strGroupName]['participants_count'] -= 1
+        # システムメッセージの作成
+        strMessage = '"' + self.strUserName + '" left. there are ' + str( ChatConsumer.rooms[self.strGroupName]['participants_count'] ) + ' participants'
+        # グループ内の全コンシューマーにメッセージ拡散送信（受信関数を'type'で指定）
+        data = {
+            'type': 'chat_message', # 受信処理関数名
+            'message': strMessage, # メッセージ
+            'username': USERNAME_SYSTEM, # ユーザー名
+            'datetime': datetime.datetime.now().strftime( '%Y/%m/%d %H:%M:%S' ), # 現在時刻
+        }
+        await self.channel_layer.group_send( self.strGroupName, data )
+
+        # 参加者がゼロのときは、ルーム管理からルームの削除
+        if( 0 == ChatConsumer.rooms[self.strGroupName]['participants_count'] ):
+            del ChatConsumer.rooms[self.strGroupName]
+
         # ルーム名を空に
         self.strGroupName = ''
+        
